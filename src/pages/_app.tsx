a58bfcaaef9c931d30a1c835257eb9a2d4661ada
Felix Pacheco <devfex.19@gmail.com>
2022-09-16 17:25:17 -0400

    Renderiza Navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../globals.css';
 import type { AppProps } from 'next/app';
+import Navbar from '@organisms/Navbar';
+import { useRouter } from 'next/router';
+import paths from '@helpers/paths';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    const { pathname } = useRouter();
+    return (
+        <>
+            {pathname.includes(paths.landing.index) && <Navbar />}
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
