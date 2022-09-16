514bea04272526de5582d4b5017ab7f779f5fa5a
Felix Pacheco <devfex.19@gmail.com>
2022-09-16 17:27:26 -0400

    Implementa molécula WhyChooseOption

---
+++ src/UI/molecules/WhyChooseOption.tsx
@@ -0,0 +1,18 @@
+import { ChooseOption } from '@organisms/WhyChoose';
+import Image from 'next/image';
+import React from 'react';
+
+const WhyChooseOption = ({ imageSrc, title, description }: ChooseOption) => (
+    <div>
+        <Image
+            src={imageSrc}
+            width={72}
+            height={72}
+        />
+
+        <h3 className="mt-6 mb-2 text-[23px] leading-[62px] text-primary-dark-blue">{title}</h3>
+        <p className="leading-[26px] text-primary-gray -tracking-[0.04em]">{description}</p>
+    </div>
+);
+
+export default WhyChooseOption;
