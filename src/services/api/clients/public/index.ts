d0c4cca863bbe3684bc3c9c82799b835ef6e2f07
Felix Pacheco <devfex.19@gmail.com>
2022-09-17 15:48:19 -0400

    Agrega nuevo servicio API (createNewArticle)

--- src/services/api/clients/public/index.ts
+++ src/services/api/clients/public/index.ts
@@ -1,12 +1,15 @@
 import apiRoutes from '@services/api/apiRoutes';
 import network from '@services/network';
 import { CustomResponse } from '@types';
-import { Article_ResT } from './interfaces';
+import { Article, FormArticle_ReqT } from './interfaces';
 
 const { publicRoutes } = apiRoutes;
 
 const authApiMethods = {
-    getAllArticles: async () => (await network.get<CustomResponse<Article_ResT>>(publicRoutes.getAllArticles)).data,
+    getAllArticles: async () => (await network.get<CustomResponse<Article[]>>(publicRoutes.articles)).data,
+    createNewArticle: async (newArticle: FormArticle_ReqT) => (
+        await network.post<CustomResponse<Article>>(publicRoutes.articles, newArticle)
+    ).data,
 };
 
 export default authApiMethods;
