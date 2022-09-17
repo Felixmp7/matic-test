e9d993fa07320da62f76b8c7b1e964fd43e3787c
Felix Pacheco <devfex.19@gmail.com>
2022-09-17 09:48:41 -0400

    Exporta HeroTitle

---
+++ src/UI/atoms/HeroTitle.tsx
@@ -0,0 +1,11 @@
+const HeroTitle = () => (
+    <h1
+        className="text-primary-dark-blue text-[53px] leading-[62px]"
+    >
+        Next generation
+        <br />
+        digital banking
+    </h1>
+);
+
+export default HeroTitle;
