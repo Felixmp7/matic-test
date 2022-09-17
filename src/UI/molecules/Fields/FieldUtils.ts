ddf0b0cba1336bbff1bf3455ec8c3ce3f738f5da
Felix Pacheco <devfex.19@gmail.com>
2022-09-17 15:48:58 -0400

    Exporta inputStyles

---
+++ src/UI/molecules/Fields/FieldUtils.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/prefer-default-export
+export const inputStyles = {
+    basic: 'block px-3.5 py-2.5 w-full h-12 text-sm bg-white rounded-md border appearance-none focus:outline-none focus:ring-0',
+    noError: 'border-[#D1D5DB] focus:border-gray-600',
+    error: 'text-red-400 border-red-400 focus:border-red-400',
+};
