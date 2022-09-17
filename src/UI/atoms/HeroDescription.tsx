fba16ba66c4e1dd4756476304182e652f1a51e00
Felix Pacheco <devfex.19@gmail.com>
2022-09-17 09:49:10 -0400

    Exporta HeroDescription

---
+++ src/UI/atoms/HeroDescription.tsx
@@ -0,0 +1,11 @@
+const HeroDescription = () => (
+    <p
+        className="w-[446px] text-[18px] leading-[28.5px] text-primary-gray font-light mt-[30px] -tracking-[0.01em] mb-[37px]"
+    >
+        Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving.
+        <br />
+        budgeting, nvesting, and much more.
+    </p>
+);
+
+export default HeroDescription;
