726cb91c4b68774a115b0bbfb9950afa67a4f885
Felix Pacheco <devfex.19@gmail.com>
2022-09-17 09:48:48 -0400

    Exporta HeroFrame

---
+++ src/UI/molecules/HeroFrames.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import phonesFrame from 'public/assets/backgrounds/frames/phones-frame.svg';
+import mainFrame from 'public/assets/backgrounds/frames/main-section-frame.svg';
+
+const HeroFrames = () => (
+    <>
+        <div className="absolute top-0 -right-10">
+            <Image
+                src={mainFrame}
+                alt="main frame"
+                width={1100.96}
+                height={680}
+            />
+        </div>
+        <div className="absolute top-0 -right-16">
+            <Image
+                src={phonesFrame}
+                alt="phones frame"
+                width={767}
+                height={818}
+
+            />
+        </div>
+    </>
+);
+
+export default HeroFrames;
