c0f098b58166f862f459e1b383fc737c72543464
Felix Pacheco <devfex.19@gmail.com>
2022-09-17 09:49:32 -0400

    Importa HeroSection y agrega overflow al container principal

--- src/pages/easybank/index.tsx
+++ src/pages/easybank/index.tsx
@@ -1,10 +1,12 @@
 import Footer from '@organisms/Footer';
 import WhyChoose from '@organisms/WhyChoose';
+import HeroSection from '@templates/HeroSection';
 import Layout from '@templates/layouts/Layout';
 import type { NextPage } from 'next';
 
 const EasyBankHomePage: NextPage = () => (
-    <Layout headerTitle="EasyBank">
+    <Layout headerTitle="EasyBank" className="overflow-hidden">
+        <HeroSection />
         <WhyChoose />
         <Footer />
     </Layout>
