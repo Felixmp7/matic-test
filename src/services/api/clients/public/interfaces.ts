a55fd3b49b51e72b44de9ff7d58272b0d549935f
Felix Pacheco <devfex.19@gmail.com>
2022-09-17 12:54:59 -0400

    Exporta interfaces

---
+++ src/services/api/clients/public/interfaces.ts
@@ -0,0 +1,16 @@
+import { DateString } from '@types';
+
+//  _ReqT = Request Type
+//  _ResT = Response Type
+
+export type Article = {
+    id: string,
+    author: string,
+    title: string,
+    content: string,
+    imageUrl: string,
+    createdAt: DateString,
+    updatedAt: DateString
+}
+
+export type Article_ResT = Article[]
