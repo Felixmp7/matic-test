e1165f83a77435a14ac248c01c0ebb855c207865
Felix Pacheco <devfex.19@gmail.com>
2022-09-17 15:47:27 -0400

    Renombra ArticlesSection

--- src/pages/easybank/index.tsx
+++ src/pages/easybank/index.tsx
@@ -3,13 +3,13 @@ import WhyChooseSection from '@templates/WhyChooseSection';
 import HeroSection from '@templates/HeroSection';
 import Layout from '@templates/layouts/Layout';
 import type { NextPage } from 'next';
-import LatestArticleSection from '@templates/LatestArticles';
+import ArticleSection from '@templates/LatestArticles';
 
 const EasyBankHomePage: NextPage = () => (
     <Layout headerTitle="EasyBank" className="overflow-hidden">
         <HeroSection />
         <WhyChooseSection />
-        <LatestArticleSection />
+        <ArticleSection />
         <Footer />
     </Layout>
 );
