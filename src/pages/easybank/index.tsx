70d4d95dfcf8d403679b306731e897174c1fa80b
Felix Pacheco <devfex.19@gmail.com>
2022-09-17 12:56:08 -0400

    Importa nuevo template LatestArticleSection

--- src/pages/easybank/index.tsx
+++ src/pages/easybank/index.tsx
@@ -3,11 +3,13 @@ import WhyChooseSection from '@templates/WhyChooseSection';
 import HeroSection from '@templates/HeroSection';
 import Layout from '@templates/layouts/Layout';
 import type { NextPage } from 'next';
+import LatestArticleSection from '@templates/LatestArticles';
 
 const EasyBankHomePage: NextPage = () => (
     <Layout headerTitle="EasyBank" className="overflow-hidden">
         <HeroSection />
         <WhyChooseSection />
+        <LatestArticleSection />
         <Footer />
     </Layout>
 );
