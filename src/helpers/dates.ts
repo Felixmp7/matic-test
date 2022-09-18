f9e51fcaf593c262b4cc3380623198911039b711
Felix Pacheco <devfex.19@gmail.com>
2022-09-18 08:11:58 -0400

    Exporta funciones helpers de dates

--- src/helpers/dates.ts
+++ src/helpers/dates.ts
@@ -1,10 +1,7 @@
-/* eslint-disable import/prefer-default-export */
-// Turn your strings into dates, and then subtract them
-// to get a value that is either negative, positive, or zero.
-
 type GenericArrayWithUpdatedDate = {
     updatedAt: string,
 }
+
 export const sortByDate = <T extends GenericArrayWithUpdatedDate>(array: T[]) => array.sort(
     (firstElement, secondElement) => {
         const date1 = new Date(secondElement.updatedAt);
@@ -13,3 +10,5 @@ export const sortByDate = <T extends GenericArrayWithUpdatedDate>(array: T[]) =>
         return date1.getTime() - date2.getTime();
     },
 );
+
+export const parseDate = (date: Date) => date.toLocaleDateString();
