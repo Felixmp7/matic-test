71b3fe45ea8dbc3996b66b7d54c743591bae6477
Felix Pacheco <devfex.19@gmail.com>
2022-09-18 08:16:40 -0400

    Implementa Organismo de Tabla de Artículos

---
+++ src/UI/organisms/ArticlesTable/index.tsx
@@ -0,0 +1,34 @@
+import Pagination from '@molecules/Pagination';
+import Table from '@molecules/Table';
+import { Article } from '@services/api/clients/public/interfaces';
+import useArticlesTable from './useArticlesTable';
+import usePagination from './usePagination';
+
+type Props = {
+    articles: Article[],
+    mutateLatestArticles: () => void,
+}
+
+const ArticlesTable = ({ articles, mutateLatestArticles }: Props) => {
+    const { results, rowsPerPage } = useArticlesTable(articles);
+    const { currentPage, totalPages, handleSetCurrentPage } = usePagination(articles);
+
+    const resultsPerPage = results.slice(currentPage * rowsPerPage, (currentPage * rowsPerPage) + rowsPerPage);
+
+    return (
+        <>
+            {resultsPerPage.length > 0 && (
+                <Table results={resultsPerPage} mutateLatestArticles={mutateLatestArticles} />
+            )}
+            <div className="mt-12">
+                <Pagination
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    handleSetCurrentPage={handleSetCurrentPage}
+                />
+            </div>
+        </>
+    );
+};
+
+export default ArticlesTable;
