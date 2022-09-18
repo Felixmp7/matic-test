a482f8343466705e1c9572412fb213f06dccbed6
Felix Pacheco <devfex.19@gmail.com>
2022-09-18 09:33:08 -0400

    Ajusta grid responsive

--- src/logicalExercises/index.tsx
+++ src/logicalExercises/index.tsx
@@ -3,7 +3,7 @@ import FizzBuzzExercise from './FizzBuzzExercise';
 import WordsCounterExercise from './WordsCounterExercise';
 
 const LogicalExercises = () => (
-    <div className="grid w-4/5 grid-cols-3 mx-auto mt-10 gap-x-5">
+    <div className="grid w-4/5 mx-auto mt-10 lg:grid-cols-2 xl:grid-cols-3 gap-x-5">
         <FibonacciExercise />
         <WordsCounterExercise />
         <FizzBuzzExercise />
