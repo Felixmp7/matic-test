73eea580701dfb3ae87a94d234979115b3e4fdb6
Felix Pacheco <devfex.19@gmail.com>
2022-09-19 11:34:33 -0400

    Maneja estado de fetch en borrar articulo

--- src/UI/atoms/DeleteArticle.tsx
+++ src/UI/atoms/DeleteArticle.tsx
@@ -1,30 +1,45 @@
 import { generalSuccessResponseCode } from '@helpers/constants';
 import API from '@services/api';
+import { useState } from 'react';
+import Spinner from './Spinner';
 
-type Props = {
-    mutateLatestArticles: () => void,
-    articleId: string,
-}
-
-const handleDeleteArticle = async ({ articleId, mutateLatestArticles }: Props) => {
+const deleteArticle = async (articleId: string) => {
     try {
         const response = await API.public.deleteArticle(articleId);
-        if (response.statusCode === generalSuccessResponseCode) {
-            mutateLatestArticles();
-        }
+        return response.statusCode === generalSuccessResponseCode;
     } catch (error) {
-        console.log(error);
+        return false;
     }
 };
 
-const DeleteArticle = ({ articleId, mutateLatestArticles }: Props) => (
-    <button
-        type="button"
-        className="text-primary-red"
-        onClick={() => handleDeleteArticle({ articleId, mutateLatestArticles })}
-    >
-        Delete
-    </button>
-);
+type Props = {
+    articleId: string,
+    mutateLatestArticles: () => void,
+}
+
+const DeleteArticle = ({ articleId, mutateLatestArticles }: Props) => {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const handleDeleteArticle = async () => {
+        setIsSubmitting(true);
+        const fetchSucceded = await deleteArticle(articleId);
+        if (fetchSucceded) {
+            mutateLatestArticles();
+        }
+        setIsSubmitting(false);
+    };
+
+    return (
+        <button
+            type="button"
+            className="text-center text-primary-red"
+            onClick={handleDeleteArticle}
+        >
+            {isSubmitting
+                ? <Spinner className="w-2.5 h-2.5 dark:text-primary-gray fill-primary-cyan" />
+                : 'Delete'}
+        </button>
+    );
+};
 
 export default DeleteArticle;
