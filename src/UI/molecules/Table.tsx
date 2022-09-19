245f1cbaf563615e786e00b477070736717bb288
Felix Pacheco <devfex.19@gmail.com>
2022-09-19 11:52:16 -0400

    Ajusta estilos de tabla

--- src/UI/molecules/Table.tsx
+++ src/UI/molecules/Table.tsx
@@ -22,15 +22,15 @@ const Table = ({ results, mutateLatestArticles }: Props) => {
         setDefaultFormValues(defaultValues);
     };
     return (
-        <div className="overflow-hidden border rounded-lg border-primary-gray-200 ">
-            <table className="w-full table-fixed text-primary-gray-500">
+        <div className="w-full overflow-hidden border rounded-lg border-primary-gray-200">
+            <table className="w-full table-auto text-primary-gray-500">
                 <thead>
                     <tr className="h-10 text-left bg-primary-gray-400 text-[12px] leading-4 tracking-[0.05em] border-b">
-                        <th className="px-6 py-3 font-light">AUTHOR NAME</th>
-                        <th className="px-6 py-3 font-light">TITLE</th>
-                        <th className="px-6 py-3 font-light">CONTENT</th>
-                        <th className="px-6 py-3 font-light">DATE</th>
-                        <th className="px-6 py-3" />
+                        <th className="py-3 pl-6 font-light">AUTHOR NAME</th>
+                        <th className="py-3 pl-6 font-light">TITLE</th>
+                        <th className="py-3 pl-6 font-light">CONTENT</th>
+                        <th className="py-3 pl-6 font-light">DATE</th>
+                        <th className="py-3 pl-6" />
                     </tr>
                 </thead>
                 <tbody>
@@ -38,11 +38,11 @@ const Table = ({ results, mutateLatestArticles }: Props) => {
                         const isOddRow = index % 2 !== 0;
                         return (
                             <tr key={article.id} className={clsx('h-10 text-[14px] leading-5', isOddRow && 'bg-primary-gray-400')}>
-                                <td className="px-6 py-3 font-medium truncate text-primary-dark-blue">{article.author}</td>
-                                <td className="px-6 py-3 truncate -tracking-[0.02em]">{article.title}</td>
-                                <td className="px-6 py-3 truncate -tracking-[0.02em]">{article.content}</td>
-                                <td className="px-6 py-3">{parseDate(new Date(article.updatedAt))}</td>
-                                <td className="px-6 py-3 space-x-4">
+                                <td className="py-3 pl-6 font-medium truncate w-[152px] text-primary-dark-blue">{article.author}</td>
+                                <td className="max-w-[313px] pl-6 py-3 truncate -tracking-[0.02em]">{article.title}</td>
+                                <td className="max-w-[383px] pl-6 py-3 truncate -tracking-[0.02em]">{article.content}</td>
+                                <td className="py-3 pl-6">{parseDate(new Date(article.updatedAt))}</td>
+                                <td className="py-3 pl-6 space-x-4">
                                     <DeleteArticle
                                         articleId={article.id}
                                         mutateLatestArticles={mutateLatestArticles}
