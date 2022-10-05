6e9c0ef991c7824f829f2e859ac85730b5ac1208
Felix Pacheco <devfex.19@gmail.com>
2022-10-05 16:10:51 -0400

    Ajusta skeleton

--- src/UI/organisms/LatestArticles.tsx
+++ src/UI/organisms/LatestArticles.tsx
@@ -10,7 +10,7 @@ const LatestArticles = ({ articles, isLoading }: Props) => (
     <div className="grid gap-10 mobileLg:grid-cols-2 place-items-center tabletLg:place-items-start tabletLg:grid-cols-4">
         {isLoading || !articles
             ? Array.from(Array(4).keys()).map((value) => (
-                <div key={value} className="overflow-hidden bg-white rounded-lg animate-pulse">
+                <div key={value} className="overflow-hidden bg-white rounded-lg animate-pulse w-full max-w-[248px]">
                     <div className="w-full h-40 bg-gray-200" />
                     <div className="p-[26px] space-y-4">
                         <div className="w-20 h-2 bg-gray-200 rounded-lg" />
